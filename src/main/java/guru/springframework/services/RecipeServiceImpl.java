bf362f5bb5efb810ebab679aab6f57d30af07432
Danijel Petanovic <danijel.petanovic@ericsson.com>
2019-07-23 00:32:15 +0200

    Adding save command object to service and adding IT tests for that.

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -3,19 +3,28 @@ package guru.springframework.services;
 import java.util.HashSet;
 import java.util.Set;
 
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.converters.RecipeCommandToRecipe;
+import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe,
+                             RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -31,5 +40,15 @@ public class RecipeServiceImpl implements RecipeService {
     public Recipe findById(Long id) {
 
         return recipeRepository.findById(id).orElseThrow(() -> new RuntimeException("Recipe Not Found"));
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId: " + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
